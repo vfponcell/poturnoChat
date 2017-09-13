a49c15e0a321dee29a6382eabfa36ea30a61a5cd
Vitor Poncell <vitorponcell@hotmail.com>
2017-09-13 14:59:03 -0300

    adicionado toolbar no main criado thema padrao para botoes

--- PoturnoChat/app/src/main/java/com/poturno/poturnochat/activity/MainActivity.java
+++ PoturnoChat/app/src/main/java/com/poturno/poturnochat/activity/MainActivity.java
@@ -3,6 +3,10 @@ package com.poturno.poturnochat.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,28 +18,44 @@ public class MainActivity extends AppCompatActivity {
 
     private Button logout;
     private FirebaseAuth firebaseAuth;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logout = (Button)findViewById(R.id.btn_logout);
+        firebaseAuth = FirebaseConfig.getFirebaseAuth();
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                firebaseAuth = FirebaseConfig.getFirebaseAuth();
-                firebaseAuth.signOut();
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-                openLogin();
 
-            }
-        });
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_logout:
+                openLogin();
+                return true;
+            case R.id.item_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private void openLogin(){
+        firebaseAuth.signOut();
         Intent intent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(intent);
         finish();
