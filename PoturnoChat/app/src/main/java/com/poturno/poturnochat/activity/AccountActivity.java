2c93c4c051ab71313735329305ff18de1b7c38fa
Vitor Poncell <vitorponcell@hotmail.com>
2017-09-30 23:36:43 -0300

    criado as strings values e activites config e account

---
+++ PoturnoChat/app/src/main/java/com/poturno/poturnochat/activity/AccountActivity.java
@@ -0,0 +1,15 @@
+package com.poturno.poturnochat.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.poturno.poturnochat.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
---
+++ PoturnoChat/app/src/main/java/com/poturno/poturnochat/activity/ConfigActivity.java
@@ -0,0 +1,44 @@
+package com.poturno.poturnochat.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.poturno.poturnochat.R;
+import com.poturno.poturnochat.helper.Preferences;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    private TextView name;
+    private TextView email;
+    private Button account;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+
+        name = (TextView)findViewById(R.id.txt_name);
+        email = (TextView)findViewById(R.id.txt_email);
+        account = (Button)findViewById(R.id.btn_conta);
+
+        Preferences preferences = new Preferences(ConfigActivity.this);
+        name.setText(preferences.getName());
+        email.setText(preferences.getEmail());
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAccount();
+            }
+        });
+    }
+
+    private void openAccount(){
+        Intent intent = new Intent(ConfigActivity.this,AccountActivity.class);
+        startActivity(intent);
+    }
+}
