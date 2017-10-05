a6f6ad99e38789261dc0797da5ad9b31b0279c65
Iago Belo de Oliveira Vieira <iagobelo.oliveira@gmail.com>
2017-10-05 16:35:03 -0300

    BitmapUtils.

---
+++ PoturnoChat/app/src/main/java/com/poturno/poturnochat/utils/BitmapUtils.java
@@ -0,0 +1,32 @@
+package com.poturno.poturnochat.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Created by Iago Belo on 31/07/17.
+ */
+
+public final class BitmapUtils {
+
+    private BitmapUtils() {
+
+    }
+
+    public static String encodeToBase64(Bitmap image, Bitmap.CompressFormat compressFormat,
+                                        int quality) {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        image.compress(compressFormat, quality, byteArrayOS);
+
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
+    }
+
+    public static Bitmap decodeBase64(String input) {
+        byte[] decodedBytes = Base64.decode(input, 0);
+
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+}
